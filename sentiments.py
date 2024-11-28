537eee7bd6df22a1bb498d490dcd770a78e1172e
PulchritudinousHaese <micah.skazhenyuk@mail.utoronto.ca>
2024-11-27 20:56:39 -0500

    Sentiment 1.0

--- sentiments.py
+++ sentiments.py
@@ -1,9 +1,134 @@
+from config import NEWS_API_KEY
+data_path = 'stock-predictor/data/sentiments.csv'
+
 import requests
+import csv
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
-API_KEY = 'your_finnhub_api_key'
-symbol = 'BSCW'
-url = f"https://finnhub.io/api/v1/news-sentiment?symbol={BPT}&token={ct2huqhr01qiurr3u6rgct2huqhr01qiurr3u6s0}"
+vectorizer = TfidfVectorizer()
 
-response = requests.get(url)
-sentiment_data = response.json()
-print(sentiment_data)
+def get_news(stock: str, api_key: str, count=10) -> list[dict]:
+    """Get the news for a given stock from the News API.
+
+    Most recently published count articles are returned.
+
+    :param stock: The stock to get news for.
+    :param api_key: The News API key.
+    :param count: The number of articles to get.
+    :return: The news articles.
+    """
+    query = f"{stock} stock OR {stock} OR {stock} earnings OR {stock} market news OR {stock} stock price OR {stock} stock news"
+    url = f'https://newsapi.org/v2/everything?q={query}&language=en&sortBy=publishedAt&pageSize={count}&apiKey={api_key}'
+    response = requests.get(url)
+    return response.json()['articles']
+
+def load_data(data_path: str) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Load the data from a CSV file and return train, validation, and test sets.
+
+    Pre process it with a vectorizer.
+
+    :param data_path: The path to the CSV file.
+    :return: Data in numpy arrays in three sets: train, validation, and test.
+    """
+    global vectorizer
+
+    X_pre =[]
+    X = [] # Articles
+    y = [] # Sentiments
+
+    with open(data_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            X_pre.append(clean_article(row[1]))
+            y.append(row[0])
+
+    X = vectorizer.fit_transform(X_pre)
+    y = np.array(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def clean_article(article: str) -> str:
+    """Clean the article by removing special characters and converting to lowercase.
+
+    :param article: The article to clean.
+    :return: The cleaned article.
+    """
+    return ''.join(e for e in article if e.isalnum() or e.isspace()).lower()
+
+def train_classifier(X_train: np.ndarray, y_train: np.ndarray, X_val: np.ndarray, y_val: np.ndarray, depth=None, estimators=None) -> RandomForestClassifier:
+    """Train a classifier on the given data.
+
+    Vary the depth of the tree to 5 levels.
+
+    :param X_train: The training data.
+    :param y_train: The training labels.
+    :return: The trained classifier.
+    """
+    if depth is None:
+        max_depths = [int(i) for i in range(100, 150, 2)]
+    else:
+        max_depths = [depth]
+
+    if estimators is None:
+        n_estimators = [int(i) for i in range(100, 300, 100)]
+    else:
+        n_estimators = [estimators]
+
+    accuracies = []
+
+    best_accuracy = float('-inf')
+    best_clf = None
+    best_estimator = None
+
+    for depth in max_depths:
+        for estimator in n_estimators:
+            clf = RandomForestClassifier(n_estimators=estimator, max_depth=depth, random_state=42)
+            # clf = DecisionTreeClassifier(max_depth=depth, random_state=42)
+            clf.fit(X_train, y_train)
+
+            accuracy = clf.score(X_val, y_val)
+            accuracies.append(accuracy)
+
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_clf = clf
+                best_estimator = estimator
+    
+    print(f'Best accuracy: {best_accuracy} at depth {best_clf.max_depth} at {best_estimator} estimators')
+
+    if depth is None and estimators is None:
+        plt.plot(max_depths, accuracies[:len(max_depths)], label='100 estimators')
+        plt.plot(max_depths, accuracies[len(max_depths):], label='200 estimators')
+        plt.xlabel('Max Depth')
+        plt.ylabel('Accuracy')
+        plt.title('Accuracy vs. Max Depth')
+        plt.show()
+
+    return best_clf
+    
+
+if __name__ == '__main__':
+    X_train, y_train, X_val, y_val, X_test, y_test = load_data(data_path)
+    # Best rn, 132/100 0.76
+    clf = train_classifier(X_train, y_train, X_val, y_val, estimators=100)
+    print(clf.score(X_val, y_val))
+
+    print(clf.score(X_test, y_test))
+
+
+    news = get_news('AAPL', NEWS_API_KEY, count=1)
+
+    for article in news:
+        print(article['title'])
+        print(clf.predict(vectorizer.transform([article['title']])))
+        print(clf.predict_proba(vectorizer.transform([article['title']])))
+
+    
